edafa7068256f16213399ac68aaaec94abd531ad
Marcus Ngooi <mngooi@gmail.com>
2021-10-31 23:28:49 -0400

    Added animations, sounds, particle effects

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,22 +10,30 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float groundCheckRadius = 0.15f;
     [SerializeField] private Transform groundCheckPos;
     [SerializeField] private LayerMask whatIsGround;
+    [SerializeField] private GameObject deathSoundContainer;
 
     // Private variables
     private Rigidbody2D rBody;
+    private Animator anim;
     private bool isGrounded = false;
-    private float timer = 0;
+    private bool isDead = false;
+    AudioSource jumpSound;
+    AudioSource deathSound;
+
 
     // Start is called before the first frame update
     void Start()
     {
         rBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        jumpSound = GetComponent<AudioSource>();
+        deathSound = deathSoundContainer.GetComponentInChildren<AudioSource>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
+
     }
 
     private void FixedUpdate()
@@ -34,11 +42,8 @@ public class PlayerController : MonoBehaviour
         //float horiz = Input.GetAxis("Horizontal");
         //rBody.velocity = new Vector2(horiz * speed, rBody.velocity.y);
 
-
         isGrounded = GroundCheck();
         rBody.velocity = new Vector2(speed, rBody.velocity.y);
-
-
 
         // Jump code
         if (isGrounded && Input.GetAxis("Jump") > 0)
@@ -47,13 +52,21 @@ public class PlayerController : MonoBehaviour
             {
                 rBody.AddForce(new Vector2(0.0f, -jumpForce));
                 isGrounded = false;
+                jumpSound.Play();
             }
             if (rBody.gravityScale > 0)
             {
                 rBody.AddForce(new Vector2(0.0f, jumpForce));
                 isGrounded = false;
+                jumpSound.Play();
             }            
         }
+
+        // Communicate with the animator
+        anim.SetFloat("xVelocity", Mathf.Abs(rBody.velocity.x));
+        anim.SetFloat("yVelocity", rBody.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+        anim.SetBool("isDead", isDead);
     }
 
     private bool GroundCheck()
@@ -73,14 +86,17 @@ public class PlayerController : MonoBehaviour
         // "Kill" player if they touch a spike
         if (other.gameObject.CompareTag("Spike"))
         {
-            Debug.Log(timer);
-            timer = 0;
-            this.transform.position = new Vector3(-10.27f, -4.4f, 0.0f);
-            if (rBody.gravityScale < 0)
-            {
-                rBody.gravityScale *= -1;
-                Flip();
-            }
+            isDead = true;
+            rBody.constraints = RigidbodyConstraints2D.FreezeAll;
+            deathSound.Play();
+            //this.transform.position = new Vector3(-10.27f, -4.4f, 0.0f);
+            //if (rBody.gravityScale < 0)
+            //{
+            //    rBody.gravityScale *= -1;
+            //    Flip();
+            //}
+
+            
         }
     }
 }
